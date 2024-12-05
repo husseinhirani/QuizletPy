972a65c79efd0b4faadbad04c70b25c568d240ec
husseinhirani <hiraniusaco@gmail.com>
2024-12-04 18:14:51 -0800

    Added keystrokes

--- guistuff.py
+++ guistuff.py
@@ -1,10 +1,12 @@
 import tkinter
 import parse
-
+import tkinter.font as tkFont
 
 index=0
 termodef=True
-def flipCard():
+
+#functions
+def flipCard(event=None):
     global termodef
     global index
     if termodef:
@@ -13,14 +15,15 @@ def flipCard():
     else:
         flashcard["text"]= terms[index]
     termodef= not termodef
-def nextCard():
+def nextCard(event=None):
     global index
     global termodef
     if(index < len(terms)-1):
         index +=1
         flashcard["text"] = terms[index]
         termodef = True
-def prevCard():
+def prevCard(event=None):
+    
     global index
     global termodef
     if(index !=0 ):
@@ -31,14 +34,22 @@ def prevCard():
 terms = parse.term()
 definition = parse.defs()
 root = tkinter.Tk()
+normal_font = tkFont.Font(family="Arial", size=20, weight=tkFont.NORMAL)
 
-
-
-flashcard = tkinter.Button(text=terms[index], command=flipCard)
+#defining UI elements
+flashcard = tkinter.Button(text=terms[index], command=flipCard, font=normal_font)
 right = tkinter.Button(text=">", command = nextCard)
 left = tkinter.Button(text="<", command = prevCard)
 
 
+#Keyboard Controls
+root.bind("<Left>", prevCard)
+root.bind("<Right>", nextCard)
+root.bind("<space>", flipCard)
+
+
+
+#pack
 flashcard.pack()
 right.pack()
 left.pack()
